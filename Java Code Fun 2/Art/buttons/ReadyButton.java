8c58a10dbbe0483c26cae6697545641c1e6cac58
Akoes27 <159651293+Akoes27@users.noreply.github.com>
2024-11-16 12:04:15 -0500

    Added dimensions to button
    the button is still overscaling );

--- Java Code Fun 2/Art/buttons/ReadyButton.java
+++ Java Code Fun 2/Art/buttons/ReadyButton.java
@@ -2,6 +2,7 @@ package Art.buttons;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,27 +21,39 @@ public class ReadyButton extends BaseButton implements ActionListener {
     private boolean clicked =false;
     private Player player;
     private static final Color grayness = new Color(184,181,194);
+    private ImageIcon current;
     private static final ImageIcon notReady = new ImageIcon("Java-Code-Fun/Java Code Fun 2/Art/buttons/notReadyButton.png");
     private static final ImageIcon ready = new ImageIcon("Java-Code-Fun/Java Code Fun 2/Art/buttons/readyButton.png");
-    
+    private static final Dimension base = new Dimension(ready.getIconWidth(),ready.getIconHeight());
     
         public ReadyButton(Player player){
             super(notReady);
             //super("Ready",new Color(140,140,140));
         setPreferredSize(new Dimension(80,30));
         this.player=player;
-        setOpaque(true);
+        setOpaque(false);
 
         addActionListener(this);
         //setBorder(new LineBorder(Color.black,3, true) );
         //setBackground(grayness);
-        setPreferredSize(getSize());
+        setSize(base);
+        setPreferredSize(base);
+        setBorderPainted(false);
+        setMaximumSize(base);
+    }
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        //setIcon(new ImageIcon(current.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH)));
+
+        super.paintComponent(g);
     }
 
     public void prepare(){
         //setBackground(grayness);
         //paintComponent(getGraphics());
-        setIcon(new ImageIcon(ready.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_FAST)));
+        current = ready;
+        setIcon(ready);
         setEnabled(true);
 
 
@@ -61,8 +74,9 @@ public class ReadyButton extends BaseButton implements ActionListener {
 
             System.out.println("Ready Button Clicked when Ready");
         //setBackground(new Color(50,50,60));
-        setIcon(new ImageIcon(notReady.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_FAST)));
-
+        //setIcon(new ImageIcon(notReady.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_FAST)));
+        current = notReady;
+        setIcon(notReady);
         Main.onwards();
 
     }
